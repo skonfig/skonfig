ebcb372047d5bde51c3c680605aaede16f8f7a95
Dennis Camera <dennis.camera@riiengineering.ch>
2023-02-21 10:30:20 +0100

    Remove banner, inventory, shell from shell completion

--- completions/bash/cdist-completion.bash
+++ completions/bash/cdist-completion.bash
@@ -6,67 +6,10 @@ _cdist()
     prev="${COMP_WORDS[COMP_CWORD-1]}"
     prevprev="${COMP_WORDS[COMP_CWORD-2]}"
     opts="-h --help -q --quiet -v --verbose -V --version"
-    cmds="banner config inventory shell"
-
-    case "${prevprev}" in
-        shell)
-            case "${prev}" in
-                -s|--shell)
-                    shells=$(grep -v '^#' /etc/shells)
-                    COMPREPLY=( $(compgen -W "${shells}" -- ${cur}) )
-                    return 0
-                    ;;
-            esac
-            ;;
-         inventory)
-            case "${prev}" in
-                list)
-                    opts="-h --help -q --quiet -v --verbose \
-                        -I --inventory -a --all -f --file -H --host-only \
-                        -t --tag"
-                    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-                    return 0
-                    ;;
-                add-host)
-                    opts="-h --help -q --quiet -v --verbose \
-                        -I --inventory -f --file"
-                    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-                    return 0
-                    ;;
-                del-host)
-                    opts="-h --help -q --quiet -v --verbose \
-                        -I --inventory -a --all -f --file"
-                    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-                    return 0
-                    ;;
-                add-tag)
-                    opts="-h --help -q --quiet -v --verbose \
-                        -I --inventory -f --file -T --tag-file -t --taglist"
-                    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-                    return 0
-                    ;;
-                del-tag)
-                    opts="-h --help -q --quiet -v --verbose \
-                        -I --inventory -a --all -f --file -T --tag-file -t --taglist"
-                    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-                    return 0
-                    ;;
-            esac
-            ;;
-    esac
+    cmds="config"
 
     case "${prev}" in
         -*)
-            COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-            return 0
-            ;;
-        banner)
-            opts="-h --help -q --quiet -v --verbose"
-            COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-            return 0
-            ;;
-        shell)
-            opts="-h --help -q --quiet -v --verbose -s --shell"
             COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
             return 0
             ;;
@@ -78,12 +21,6 @@ _cdist()
                 -r --remote-out-dir \
                 -s --sequential --remote-copy --remote-exec -t --tag -a --all"
             COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-            return 0
-            ;;
-        inventory)
-            cmds="list add-host del-host add-tag del-tag"
-            opts="-h --help -q --quiet -v --verbose"
-            COMPREPLY=( $(compgen -W "${opts} ${cmds}" -- ${cur}) )
             return 0
             ;;
         *)
