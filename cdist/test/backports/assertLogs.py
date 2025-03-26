65d9759fbfe29019e36189f8e165014dc9f7fdae
Dennis Camera <dennis.camera@riiengineering.ch>
2025-03-26 16:41:25 +0100

    Add missing copyright headers

--- cdist/test/backports/assertLogs.py
+++ cdist/test/backports/assertLogs.py
@@ -1,4 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# backported from Python stdlib by
+# 2025 Dennis Camera (dennis.camera at riiengineering.ch)
+#
+# This file is part of skonfig.
+#
+# skonfig is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# skonfig is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with skonfig. If not, see <http://www.gnu.org/licenses/>.
+#
 # backport of unittest/case.py's assertLogs().
+#
 
 import logging
 
